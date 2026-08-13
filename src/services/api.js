0b07f5308c7e29f75b6c11d635d01fcecd7c4627
otaviano <0t4v14n0@gmail.com>
2026-08-13 14:47:35 -0300

    mudanca no endpoint do backend publico e free

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: 'https://nondualistically-ostensive-luella.ngrok-free.dev', 
+    baseURL: 'https://3313-2804-3fc4-61e-5500-fd3c-5afd-3c6c-e121.ngrok-free.app', 
     headers: {
         'ngrok-skip-browser-warning': 'true', // Tem que ter isso!
         'Content-Type': 'application/json'
