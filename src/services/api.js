aba22460c4f60c3e522692ec721893a837d76be2
otaviano <0t4v14n0@gmail.com>
2026-09-16 20:19:30 -0300

    subindo producao sem www

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: 'https://www.api.routriz.com.br/', 
+    baseURL: 'https://api.routriz.com.br/', 
     headers: {
         'ngrok-skip-browser-warning': 'true', // Tem que ter isso!
         'Content-Type': 'application/json'
